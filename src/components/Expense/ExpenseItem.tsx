ee19b28583817a5c4e896e9514959e9fb7c6c67b
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-01 16:39:21 +0200

    change to li

--- src/components/Expense/ExpenseItem.tsx
+++ src/components/Expense/ExpenseItem.tsx
@@ -10,16 +10,16 @@ export interface ExpenseProps {
 }
 
 function ExpenseItem({ id, date, title, amount }: ExpenseProps): JSX.Element {
-    
-
     return (
-        <Card id={id} className='expense-item'>
-            <ExpenseDate date={date}></ExpenseDate>
-            <div className='expense-item__description'>
-                <h2>{title}</h2>
-                <div className='expense-item__price'>${amount}</div>
-            </div>
-        </Card>
+        <li>
+            <Card id={id} className='expense-item'>
+                <ExpenseDate date={date}></ExpenseDate>
+                <div className='expense-item__description'>
+                    <h2>{title}</h2>
+                    <div className='expense-item__price'>${amount}</div>
+                </div>
+            </Card>
+        </li>
     );
 }
 
