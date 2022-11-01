b08b2a24ba65176c12783e77565d7e5999475b2e
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-01 16:40:21 +0200

    conditional form

--- src/components/NewExpense/ExpenseForm.tsx
+++ src/components/NewExpense/ExpenseForm.tsx
@@ -10,6 +10,8 @@ interface ExpenseFormProps {
 }
 
 function ExpenseForm({ onTitleChange, onAmountChange, onDateChange, onDataSave }: ExpenseFormProps): JSX.Element {
+    const [showForm, setShowForm] = useState(false);
+
     const [title, setTitle] = useState('');
     const [amount, setAmount] = useState(0.01);
     const [date, setDate] = useState('');
@@ -18,35 +20,44 @@ function ExpenseForm({ onTitleChange, onAmountChange, onDateChange, onDataSave }
         e.preventDefault()
         // alert(JSON.stringify({title,amount,date}))
         onDataSave && onDataSave({title,amount,date})
+        setShowForm(false)
     }
 
-    return (
-        <form onSubmit={Submit}>
-            <div className="new-expense__controls">
-                <div className="new-expense__control">
-                    <label>Title</label>
-                    <input type="text"
-                        onChange={(e) => { setTitle(e.target.value); onTitleChange && onTitleChange(e); }}
-                        value={title} />
+    if (showForm) {
+        return (
+            <form onSubmit={Submit}>
+                <div className="new-expense__controls">
+                    <div className="new-expense__control">
+                        <label>Title</label>
+                        <input type="text"
+                            onChange={(e) => { setTitle(e.target.value); onTitleChange && onTitleChange(e); }}
+                            value={title} />
+                    </div>
+                    <div className="new-expense__control">
+                        <label>Amount</label>
+                        <input type="number" min="0.01" step="0.01"
+                            onChange={(e) => { setAmount(parseInt(e.target.value)); onAmountChange && onAmountChange(e); }}
+                            value={amount} />
+                    </div>
+                    <div className="new-expense__control">
+                        <label>Date</label>
+                        <input type="date" min="2019-01-01" max="2022-12-31" 
+                            onChange={(e) => { setDate(e.target.value); onDateChange && onDateChange(e); }}
+                            value={date}/>
+                    </div>
                 </div>
-                <div className="new-expense__control">
-                    <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01"
-                        onChange={(e) => { setAmount(parseInt(e.target.value)); onAmountChange && onAmountChange(e); }}
-                        value={amount} />
+                <div className="new-expense__actions">
+                    <button onClick={()=>setShowForm(false)}>Cancel</button>
+                    <button type="submit">Add Expense</button>
                 </div>
-                <div className="new-expense__control">
-                    <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" 
-                        onChange={(e) => { setDate(e.target.value); onDateChange && onDateChange(e); }}
-                        value={date}/>
-                </div>
-            </div>
-            <div className="new-expense__actions">
-                <button type="submit">Add Expense</button>
-            </div>
-        </form>
-    );
+            </form>
+        );
+    } else {
+        return (
+            <button onClick={()=>setShowForm(true)}>Add New Expense</button>
+        );
+    }
+
 }
 
 export default ExpenseForm;
